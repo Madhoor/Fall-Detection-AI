e3d08419b8c93594fee80f24ea11339f2559db0a
Madhoor Deo <79635992+Madhoor@users.noreply.github.com>
2026-04-09 20:10:39 +0530

    Implement model download and update video source
    Added model downloading functionality and updated video capture index.

--- src/main.py
+++ src/main.py
@@ -1,25 +1,41 @@
+import os
+import urllib.request
 import cv2
-from matplotlib.pylab import angle
-import mediapipe as mp
 import numpy as np
+import mediapipe as mp
+from matplotlib.pylab import angle
 from mediapipe.tasks.python import vision
 from mediapipe.tasks import python
 from mediapipe.tasks.python.vision import drawing_utils
 from mediapipe.tasks.python.vision import drawing_styles
-
 import math
+
 
 
 prev_hip_y = None
 fall_detected = False
 
 
+MODEL_PATH = "models/pose_landmarker.task"
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1GsffmrIK38eHwp9u6UfstsDzlx53OUZ4"
 
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("📥 Model not found. Downloading...")
+        os.makedirs("models", exist_ok=True)
+        urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+        print("✅ Model downloaded successfully!")
+
+try:
+    download_model()
+except Exception as e:
+    print("❌ Failed to download model. Please download it manually.")
+    print("Error:", e)
 # Load Model        
 
 base_options = python.BaseOptions(
-    # model_asset_path='D:/Hackathon/MAAI/AI MODEL/pose_landmarker.task'
-    model_asset_path='models/pose_landmarker.task'
+    # Make sure to download the model and provide the correct path
+    model_asset_path=MODEL_PATH
 )
 
 options = vision.PoseLandmarkerOptions(
@@ -30,7 +46,8 @@ options = vision.PoseLandmarkerOptions(
 detector = vision.PoseLandmarker.create_from_options(options)
 
 # Video Feed
-cap = cv2.VideoCapture(2)
+
+cap = cv2.VideoCapture(0) # change the argument to a video file path for processing a video instead of webcam feed / or add one for external webcam
 
 def calculate_angle(p1, p2):
     dx = p2.x - p1.x
